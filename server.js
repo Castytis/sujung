88fe7d634b8a7ca5c5eb161d004ec534d1181a5c
Kastytis S <kastytis.slubelis@gmail.com>
2022-03-29 19:41:31 +0300

    db setup

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/database');
+const connectToDB = require('./config/database');
+
 const app = express();
 const PORT = 5000;
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
