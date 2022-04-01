f5841c3f9f373e422e4342f625d81addfdd077bb
Kastytis S <kastytis.slubelis@gmail.com>
2022-04-01 18:35:54 +0300

    Creating Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import React, { Fragment } from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
+import Landing from './components/layout/Landing';
+import Layout from './components/layout/Layout';
+import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Fragment>
+      <Layout>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Landing />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </Layout>
+    </Fragment>
   );
 }
 
