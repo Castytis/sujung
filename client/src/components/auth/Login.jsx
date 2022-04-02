31c4cb59f70676552481bf72d8220772f41cded3
Kastytis S <kastytis.slubelis@gmail.com>
2022-04-02 19:12:50 +0300

    Login form bootstrap

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  max-width: 480px;
+  margin: auto;
+`;
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Styles>
+      <Container>
+        <Row>
+          <Col className='p-5'>
+            <h3 className='text-center'>Prisijunkite</h3>
+            <Form>
+              <Form.Group className='mb-3' controlId='emailInput'>
+                <Form.Label className='text-center'>El. Paštas</Form.Label>
+                <Form.Control type='email' placeholder='El. Paštas' />
+              </Form.Group>
+              <Form.Group className='mb-3' controlId='passwordInput'>
+                <Form.Label>Slaptažodis</Form.Label>
+                <Form.Control type='password' placeholder='Slaptažodis' />
+              </Form.Group>
+              <Button variant='warning' className='float-start'>
+                Prisijungti
+              </Button>
+              <Button variant='danger' className='float-end'>
+                Atšaukti
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </Styles>
+  );
 };
 
 export default Login;
