70bbd3138a0499a356dd9f0238676349f7f473c0
Kastytis S <kastytis.slubelis@gmail.com>
2022-04-11 11:53:17 +0300

    Notification Reducer & Action

---
+++ client/src/store/actions/notification-action.js
@@ -0,0 +1,15 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const setNotification = (msg, msgType) => {
+  return async (dispatch) => {
+    const id = uuidv4();
+    dispatch({
+      type: 'SET_NOTIFICATION',
+      payload: { msg, msgType, id },
+    });
+
+    setTimeout(() => {
+      dispatch({ type: 'REMOVE_NOTIFICATION', payload, id });
+    }, 4000);
+  };
+};
